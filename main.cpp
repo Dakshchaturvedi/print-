34f29ea9fa6bb02add50b04d274841c517aaf6ef
Dakshchaturvedi <106811707+Dakshchaturvedi@users.noreply.github.com>
2024-04-28 09:41:45 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void printSubstrings(string str)
+{
+	for (int i = 0; i < str.length(); i++) {
+		string subStr;
+		for (int j = i; j < str.length(); j++) {
+			subStr += str[j];
+			cout << subStr << endl;
+            }
+	}
+}
+
+int main()
+{
+	string str = "abcdabc";
+	printSubstrings(str);
+	return 0;
+}
+
+// time Complexity =O(N2)
